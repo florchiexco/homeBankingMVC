ee61f39491b4ecc15ecd6a60c2f63b5ac2661faa
Jazmin <briascojazmin@gmail.com>
2020-02-04 11:28:21 -0300

    modelo agregado

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import Modelo from "./modelo.js"
+const modelo = new Modelo();
+console.log(modelo);
+console.log(modelo.cuenta);
---
+++ js/modelo.js
@@ -0,0 +1,113 @@
+
+/*class Modelo {
+    constructor() {
+        this.asistentesPredeterminados = [
+            ["juan", "juan@mail.com", "28"],
+            ["Guido", "guido@mail.com", "22"],
+            ["Mia", "mia@mail.com", "16"],
+            ["tiziano", "tiziano@mail.com", "5"],
+            ["micaela", "micaela@mail.com", "12"],
+            ["martina", "martina@mail.com", "7"],
+          ]
+    }
+    agregar( asistente ) {
+        this.asistentesPredeterminados.push( asistente )
+    }
+    obtenerAsistentes () {
+        return this.asistentesPredeterminados
+    }
+}
+
+export default Modelo*/
+//var cuenta, cuentas, amigos, servicios;
+    
+class Modelo{
+    constructor(){
+        //Datos
+         this.cuenta;
+         this.cuentas;
+         this.amigos;
+         this.servicios;
+         this.init();
+    }
+    //Metodos
+    init(){
+        console.log("INIT");
+        if(localStorage.getItem("cuentas") == null){
+            readJSONcuentas().then(asignarCuentas);
+            }
+            else{
+            this.cuentas= JSON.parse(localStorage.getItem("cuentas"));
+            }
+            
+            if(localStorage.getItem("cuenta")== null){
+            readJSONcuentas().then(asignarCuenta);
+            }
+            else{
+            this.cuenta= JSON.parse(localStorage.getItem("cuenta"));
+            }
+            
+            if(localStorage.getItem("amigos")== null){
+            readJSONcuentas().then(asignarAmigos);
+            }
+            else{
+                this.amigos= JSON.parse(localStorage.getItem("amigos"));
+            }
+            
+            if(localStorage.getItem("servicios")== null){
+            readJSONservicios().then(asignarServicios);
+            }
+            else{
+                this.servicios= JSON.parse(localStorage.getItem("servicios"));    
+            }
+    }
+
+    async readJSONcuentas(){
+        try{
+            var response= await fetch("data/cuentas.json");
+            var data = await response.json();
+            return data;
+        }
+        catch(error){
+            throw(error);
+        }
+    }
+        async readJSONservicios(){
+            try{
+                var response= await fetch("data/servicios.json");
+                var data = await response.json();
+                return data;
+            }
+            catch(error){
+                throw(error);
+            }
+        }
+        asignarCuentas(datos){
+            this.cuentas=datos;
+            console.log(this.cuentas);
+         }
+    
+        asignarServicios(datos){
+            this.servicios=datos;
+            console.log(this.servicios);
+        }
+        
+        asignarCuenta= function(){
+            this.cuenta= this.cuentas[0];
+            console.log(this.cuenta);
+        }
+        
+        asignarAmigos= function(){
+            this.amigos= this.cuentas.slice(1,3);
+            console.log(this.amigos);
+    }
+    
+    
+    
+    }
+
+export default Modelo
+
+
+
+
